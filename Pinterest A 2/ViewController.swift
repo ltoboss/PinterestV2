bc57f428b361fd79306df98b4d7db3de36bb3fe7
Sarahí Ramírez Omaña <luis_lizto@hotmail.com>
2019-05-04 21:16:55 -0500

    Initial Commit

---
+++ Pinterest A 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pinterest A 2
+//
+//  Created by Sarahí Ramírez Omaña on 5/4/19.
+//  Copyright © 2019 Luis Felipe Osuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
